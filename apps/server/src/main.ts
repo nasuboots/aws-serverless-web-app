36a20a9efd7bab0c3123cbcd2aa78f04d371a0b8
nasuboots <50765809+nasuboots@users.noreply.github.com>
2023-05-06 14:13:42 +0000

    [server] swagger ui で API 仕様書を閲覧可能にした

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,3 +1,7 @@
+// eslint-disable-next-line node/no-unpublished-import
+import fastifySwagger from '@fastify/swagger'
+// eslint-disable-next-line node/no-unpublished-import
+import fastifySwaggerUi from '@fastify/swagger-ui'
 import { TypeBoxTypeProvider } from '@fastify/type-provider-typebox'
 import { Type as t } from '@sinclair/typebox'
 import fastify from 'fastify'
@@ -6,19 +10,53 @@ const app = fastify({
   logger: true,
 }).withTypeProvider<TypeBoxTypeProvider>()
 
+await app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'API 仕様書タイトル',
+      version: '0.0.1',
+    },
+  },
+})
+await app.register(fastifySwaggerUi, {
+  routePrefix: '/spec',
+})
+
 app.get(
   '/hello',
   {
     schema: {
       querystring: t.Object({
-        name: t.String(),
+        name: t.String({ description: '名前' }),
+        type: t.Union([t.Literal('json'), t.Literal('text')], {
+          default: 'json',
+          description: 'レスポンスの種類',
+        }),
       }),
+      response: {
+        200: {
+          content: {
+            'application/json': {
+              schema: t.Object({
+                message: t.String({ description: 'メッセージ' }),
+              }),
+            },
+            'text/plain': {
+              schema: t.String({ description: 'メッセージ' }),
+            },
+          },
+        },
+      },
     },
   },
   async (req) => {
-    return Promise.resolve(`hello ${req.query.name}`)
+    const message = `hello ${req.query.name}`
+    return Promise.resolve(req.query.type === 'json' ? { message } : message)
   }
 )
+
+await app.ready()
+app.swagger()
 
 try {
   await app.listen({ port: 3000 })
